1af600fad28bf80676a7457f291bc1a9b94907c6
Ivan <zhirnovivan1991@gmail.com>
2025-01-07 16:12:38 +0300

    создали проект добавили контроллеры и пути к нашим шаблонам

--- catalog/views.py
+++ catalog/views.py
@@ -5,10 +5,8 @@ from catalog.models import Product, Contact
 
 
 def home(request):
-    # Получаем последние 5 продуктов
     latest_products = Product.objects.order_by('-created_at')[:5]
 
-    # Выводим продукты в консоль
     print("Последние 5 созданных продуктов:")
     for product in latest_products:
         print(f" - {product.name}, цена: {product.price}")
@@ -18,11 +16,8 @@ def home(request):
 
 def contact(request):
     if request.method == "POST":
-        # Получение данных из формы
         name = request.POST.get("name")
         message = request.POST.get("message")
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     contacts = Contact.objects.all()
     return render(request, 'contacts.html', {'contacts': contacts})
