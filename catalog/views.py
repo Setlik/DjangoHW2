0331eaab657bba34d081799cd62b44a2786de358
Ivan <zhirnovivan1991@gmail.com>
2025-01-15 20:00:09 +0300

    hw 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product, Contact
 
 
+# def home(request):
+#     latest_products = Product.objects.order_by('-created_at')[:5]
+#
+#     print("Последние 5 созданных продуктов:")
+#     for product in latest_products:
+#         print(f" - {product.name}, цена: {product.price}")
+#
+#     return render(request, 'home.html', {'latest_products': latest_products})
+
 def home(request):
-    latest_products = Product.objects.order_by('-created_at')[:5]
-
-    print("Последние 5 созданных продуктов:")
-    for product in latest_products:
-        print(f" - {product.name}, цена: {product.price}")
-
-    return render(request, 'home.html', {'latest_products': latest_products})
+    products = Product.objects.all()  # Получаем все товары
+    return render(request, 'home.html', {'products': products})
 
 
 def contact(request):
@@ -22,3 +26,9 @@ def contact(request):
     contacts = Contact.objects.all()
     return render(request, 'contacts.html', {'contacts': contacts})
 
+
+def product_detail(request, product_id):
+    # Получаем товар по его ID
+    product = get_object_or_404(Product, id=product_id)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
